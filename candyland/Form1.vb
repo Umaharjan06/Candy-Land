280d9dadb2ad72bf7b18198acb61defaa207224e
Umaharjan06 <161775404+Umaharjan06@users.noreply.github.com>
2024-02-29 15:18:22 -0800

    Update Form1.vb

--- candyland/Form1.vb
+++ candyland/Form1.vb
@@ -2,18 +2,34 @@
 
     Private Sub BtnStart_Click(sender As Object, e As EventArgs) Handles BtnStart.Click
         BtnStart.BackColor = Color.Green
-        '
-        '
-        '
-        ' random number to generate
-        Dim MyMin As Integer = 1, MyMax As Integer = 5, My1stRandomNumber As Integer
-        Dim Generator As System.Random = New System.Random()
-        My1stRandomNumber = Generator.Next(MyMin, MyMax + 1)
-        Lblgivenumber.Text = My1stRandomNumber
+        DisplayMove()
 
 
     End Sub
+  Function DisplayMove() As Integer
+      Dim MyMin As Integer = 1, MyMax As Integer = 5, My1stRandomNumber As Integer
+      Dim Generator As System.Random = New System.Random()
+      My1stRandomNumber = Generator.Next(MyMin, MyMax + 1)
+      If My1stRandomNumber = 1 Then
+          pnlMain.BackColor = Color.Pink
+      End If
+      If My1stRandomNumber = 2 Then
 
+          pnlMain.BackColor = Color.Blue
+      End If
+      If My1stRandomNumber = 3 Then
+
+          pnlMain.BackColor = Color.LightGreen
+      End If
+      If My1stRandomNumber = 4 Then
+
+          pnlMain.BackColor = Color.Yellow
+      End If
+      If My1stRandomNumber = 5 Then
+
+          pnlMain.BackColor = Color.Red
+      End If
+  End Function
 
     Private Sub Lblgivenumber_Click(sender As Object, e As EventArgs) Handles Lblgivenumber.Click
 
