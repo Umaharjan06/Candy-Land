303b26095f25c62e4989b4ea2112ea93cdf91b7c
ThatOneGuy0514 <emersonekyle@gmail.com>
2024-02-29 15:46:55 -0800

    Test, changed form1 to frmCandyLandMain and changes button locations around

---
+++ candyland/frmCandyLandMain.vb
@@ -0,0 +1,43 @@
+﻿Public Class frmCandyLandMain
+
+    Private Sub BtnStart_Click(sender As Object, e As EventArgs) Handles BtnStart.Click
+        BtnStart.BackColor = Color.Green
+        DisplayMove()
+
+
+    End Sub
+  Function DisplayMove() As Integer
+      Dim MyMin As Integer = 1, MyMax As Integer = 5, My1stRandomNumber As Integer
+      Dim Generator As System.Random = New System.Random()
+      My1stRandomNumber = Generator.Next(MyMin, MyMax + 1)
+      If My1stRandomNumber = 1 Then
+          pnlMain.BackColor = Color.Pink
+      End If
+      If My1stRandomNumber = 2 Then
+
+          pnlMain.BackColor = Color.Blue
+      End If
+      If My1stRandomNumber = 3 Then
+
+          pnlMain.BackColor = Color.LightGreen
+      End If
+      If My1stRandomNumber = 4 Then
+
+          pnlMain.BackColor = Color.Yellow
+      End If
+      If My1stRandomNumber = 5 Then
+
+          pnlMain.BackColor = Color.Red
+      End If
+  End Function
+
+    Function playerturn() As String
+        'display name of player whos turn it is using a for loop 
+    End Function
+
+    Private Sub Lblgivenumber_Click(sender As Object, e As EventArgs) Handles Lblgivenumber.Click
+
+    End Sub
+
+
+End Class
